f73d9a43f0f07c03e36a5cad5ada9e3ac1f16057
juwelahmedsj <shamsulislam5333@gmail.com>
2020-03-14 21:38:45 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+const age = 4;                                      //TRULY VALUE : '0'    " "   []    {}    'false'
+if(age){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
+
+
+const num = 0;                                 //FALSE VALUE : 0   ""    undefined   null    NaN
+if(num){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
+
